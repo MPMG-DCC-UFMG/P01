38c1dc18b206df438c682f19b28844aef8924449
Walter dos Santos Filho <waltersf@gmail.com>
2020-02-14 10:49:10 -0300

    Spark profile

---
+++ etc/spark.sh
@@ -0,0 +1,4 @@
+export SPARK_HOME=/opt/spark
+export PATH=/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/bin:/sbin
+export PYTHONPATH=/python:/python/lib/py4j-0.9-src.zip:
+export SPARK_LOCAL_IP=127.0.0.1
